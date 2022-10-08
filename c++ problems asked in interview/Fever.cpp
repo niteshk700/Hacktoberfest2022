3133a1c8528e197279cc63582ecfb4843764068f
Nitesh Kumar <33413866+niteshk700@users.noreply.github.com>
2022-10-08 19:15:41 +0530

    Create Fever.cpp

---
+++ c++ problems asked in interview/Fever.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int t;	cin>>t;
+
+	while(t--){
+
+	    int x;
+
+	    cin>>x;
+
+	    if(x>98){
+
+	        cout<<"yes"<<endl;
+
+	    }
+
+	    else
+
+	    {cout<<"no"<<endl;
+
+	}}
+
+	return 0;
+
+}
